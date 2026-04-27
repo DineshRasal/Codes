ba2ea264b5b8217428d506b2e65fc5dc3b817b2d
Dinesh Rasal <rushirasal2679@gmail.com>
2026-04-27 21:41:12 +0530

    .

---
+++ String/LargestString.java
@@ -0,0 +1,20 @@
+package String;
+
+public class LargestString {
+    
+    public static void main(String[] args) {
+        
+        String fruits[] ={"Apple","Banana","Mango"};
+
+        String Largest=fruits[0];
+
+        for(int i=1;i<fruits.length;i++){
+            if(Largest.compareTo(fruits[i])<0){
+
+                Largest=fruits[i];
+            }
+
+        }
+        System.out.println(Largest);
+    }
+}
